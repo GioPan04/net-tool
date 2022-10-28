3e1bdd3f16dace0436a1dd210173d45d5fcb19e3
GioPan04 <gioele@pannetto.com>
2022-10-28 20:51:49 +0200

    Dynamic input file

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,39 @@ import IP from './models/IP';
 import Subnetwork from './models/Subnetwork';
 import { checkAllocatedSpace, getH, getRequirement, getSlash } from './tools/network';
 
-const input: INetwork[] = JSON.parse(fs.readFileSync('./in.json', 'utf-8'));
+const path = process.argv[process.argv.length - 1];
 
+try {
+  const input: INetwork[] = JSON.parse(fs.readFileSync(path, 'utf-8'));
 
-
-input.forEach((n) => {
-  const subs = n.subnetworks.map<Subnetwork>((s) => {
-    const req = getRequirement(s, true);
-    const h = getH(req);
-    const slash = getSlash(h);
-
-    return { h, slash, name: s.name };
-  }).sort((a, b) => b.h - a.h);
-
-  const networkIP = IP.parseIP(n.ip);
-  checkAllocatedSpace(subs, networkIP.slash);
-  let lastIp = networkIP.addr32;
-
-  console.log("Net Name\tNet ID\t\t\tBroadcast\t\tGateway\t\t\tStart IP\t\tEnd IP\t\t");
-
-  subs.forEach((s) => {
-    let ip = new IP(lastIp, s.slash);
-    console.log(`${s.name}\t\t${IP.convertString(ip.netID)}\t\t${IP.convertString(ip.broadcast)}\t\t${IP.convertString(ip.gateway)}\t\t${IP.convertString(ip.ipRangeStart)}\t\t${IP.convertString(ip.ipRangeEnd)}\t\t`);
-    lastIp = ip.broadcast + 1;
+  input.forEach((n) => {
+    const subs = n.subnetworks.map<Subnetwork>((s) => {
+      const req = getRequirement(s, true);
+      const h = getH(req);
+      const slash = getSlash(h);
+  
+      return { h, slash, name: s.name };
+    }).sort((a, b) => b.h - a.h);
+  
+    const networkIP = IP.parseIP(n.ip);
+    checkAllocatedSpace(subs, networkIP.slash);
+    let lastIp = networkIP.addr32;
+  
+    console.log("Net Name\tNet ID\t\t\tBroadcast\t\tGateway\t\t\tStart IP\t\tEnd IP\t\t");
+  
+    subs.forEach((s) => {
+      let ip = new IP(lastIp, s.slash);
+      console.log(`${s.name}\t\t${IP.convertString(ip.netID)}\t\t${IP.convertString(ip.broadcast)}\t\t${IP.convertString(ip.gateway)}\t\t${IP.convertString(ip.ipRangeStart)}\t\t${IP.convertString(ip.ipRangeEnd)}\t\t`);
+      lastIp = ip.broadcast + 1;
+    });
+  
   });
-
-});
-
+} catch (e) {
+  if((e as any)?.code === 'ENOENT') {
+    console.error(path + ': No such file');
+    process.exit(2);
+  } else {
+    console.error(e);
+    process.exit(1);
+  }
+}
